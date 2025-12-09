1580a21dd5eecc844e65443b2a5d87a787d9ddf4
github-user-64 <wrzh17350237062@outlook.com>
2025-12-09 12:26:02 +0800

    add api

--- ModTool/ModTool/ModTool/PatchGame/PatchMessageBuffer.cs
+++ ModTool/ModTool/ModTool/PatchGame/PatchMessageBuffer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Terraria;
 
 namespace ModTool.PatchGame
@@ -10,25 +11,68 @@ namespace ModTool.PatchGame
     {
         /// <summary/>
         public delegate void GetDataEvent(MessageBuffer This, int start, int length, int messageType);
-        /// <summary>在收到数据前</summary>
+        /// <summary/>
+        public delegate bool CanGetDataEvent(MessageBuffer This, int start, int length, int messageType);
+        /// <summary>
+        /// 如果返回<see langword="false"/>那么原版方法不会被调用, 不影响<see cref="OnGetDataPr"/>和<see cref="OnGetDataPo"/>
+        /// </summary>
+        public static event CanGetDataEvent OnCanGetData = null;
+        /// <summary>
+        /// 在收到数据前
+        /// </summary>
         public static event GetDataEvent OnGetDataPr = null;
-        /// <summary>在收到数据后</summary>
+        /// <summary>
+        /// 在收到数据后
+        /// </summary>
         public static event GetDataEvent OnGetDataPo = null;
-        /// <summary>客户端收到玩家连接时//只是有玩家处于活动状态时, 其它数据可能还未同步</summary>
+        /// <summary>
+        /// 客户端收到玩家连接时//只是有玩家处于活动状态时, 其它数据可能还未同步
+        /// </summary>
         public static event Action<int> OnPlayerConnecting = null;
-        /// <summary>客户端收到玩家断开连接时</summary>
+        /// <summary>
+        /// 客户端收到玩家断开连接时
+        /// </summary>
         public static event Action<int> OnPlayerDisconnecting = null;
+
+        /// <inheritdoc/>
+        public override bool CanGetData(MessageBuffer This, int start, int length, int messageType)
+        {
+            if (OnCanGetData == null) return true;
+
+            bool ok = true;
+            foreach (CanGetDataEvent i in OnCanGetData.GetInvocationList().Cast<CanGetDataEvent>())
+            {
+                if (i == null) continue;
+
+                This.reader.BaseStream.Position = start + 1;
+                ok &= i.Invoke(This, start, length, messageType);
+            }
+
+            return ok;
+        }
 
         /// <inheritdoc/>
         public override void GetDataPrefix(MessageBuffer This, int start, int length, int messageType)
         {
-            OnGetDataPr?.Invoke(This, start, length, messageType);
+            if (OnGetDataPr == null) return;
+
+            foreach (GetDataEvent i in OnGetDataPr.GetInvocationList().Cast<GetDataEvent>())
+            {
+                This.reader.BaseStream.Position = start + 1;
+                i?.Invoke(This, start, length, messageType);
+            }
         }
 
         /// <inheritdoc/>
         public override void GetDataPostfix(MessageBuffer This, int start, int length, int messageType)
         {
-            OnGetDataPo?.Invoke(This, start, length, messageType);
+            if (OnGetDataPo == null) return;
+
+            foreach (GetDataEvent i in OnGetDataPo.GetInvocationList().Cast<GetDataEvent>())
+            {
+                This.reader.BaseStream.Position = start + 1;
+                i?.Invoke(This, start, length, messageType);
+            }
         }
 
         /// <inheritdoc/>
